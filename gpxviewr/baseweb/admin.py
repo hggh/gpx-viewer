0d2c2b8da2e4171c27f27b80ec2ca39fdda7beab
Jonas Genannt <jonas@brachium-system.net>
2024-05-26 11:36:49 +0200

    add gpxfileadmin

--- gpxviewr/baseweb/admin.py
+++ gpxviewr/baseweb/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 from baseweb.models import (
+    GPXFile,
     GPXTrack,
     GPXTrackWayPoint,
     GPXTrackSegmentPoint,
 )
+
+
+class GPXFileAdmin(admin.ModelAdmin):
+    pass
 
 
 class GPXTrackAdmin(admin.ModelAdmin):
@@ -22,3 +27,4 @@ class GPXTrackSegmentPointAdmin(admin.ModelAdmin):
 admin.site.register(GPXTrack, GPXTrackAdmin)
 admin.site.register(GPXTrackWayPoint, GPXTrackWayPointAdmin)
 admin.site.register(GPXTrackSegmentPoint, GPXTrackSegmentPointAdmin)
+admin.site.register(GPXFile, GPXFileAdmin)
