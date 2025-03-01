b4bbe58c7a2d6d7f2f9d1042c2dd5b5025281424
Jonas Genannt <jonas@brachium-system.net>
2025-03-01 11:11:41 +0100

    Django DB Migration on Startup

--- bootup.sh
+++ bootup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$GPX_VIEWR_BOOT_MODE" == "web" ]]; then
+    ./manage.py migrate
     ./manage.py collectstatic --no-input
     gunicorn --capture-output -b 0.0.0.0:8000 -w ${GUNICORN_WORKERS:-5} --threads ${GUNICORN_THREADS:-5} gpxviewr.wsgi:application
 elif [[ "$GPX_VIEWR_BOOT_MODE" == "celery" ]]; then
